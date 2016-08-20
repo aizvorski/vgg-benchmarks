733c97d7859dbf5245ca50b4956c22592b009d30
Alex Izvorski <aizvorski@gmail.com>
2016-08-19 17:58:50 -0700

    Caffe modify to do full training not just forward+backward

--- benchmark_caffe.py
+++ benchmark_caffe.py
@@ -1,4 +1,13 @@
 import subprocess
+import shutil
+
+for n in range(1,16):
+    shutil.copy("images/img0000.tiff", "images/img%04d.tiff"%(n))
 
 CAFFE = "caffe/build/tools/caffe"
-subprocess.call(CAFFE + " time -model vgg16_deploy.prototxt -iterations 100 -gpu 0", shell=True)
+
+# this benchmarks forward+backward rather than a full SGD step; it turns out there is quite a significant difference between the two
+# subprocess.call(CAFFE + " time -model vgg16_deploy.prototxt -iterations 100 -gpu 0", shell=True)
+
+subprocess.call(CAFFE + " train --solver vgg16_solver.prototxt -gpu 0", shell=True)
+
